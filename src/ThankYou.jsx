c824c941b37a501ea9dc5535b7d6d591a8afa731
Keith Ching <CAS022114@coderacademy.edu.au>
2022-05-17 11:00:31 +1000

    fixed some issues

--- src/ThankYou.jsx
+++ src/ThankYou.jsx
@@ -7,11 +7,11 @@ const ThankYou = () => {
   
   return (
     <div className="body">
-      <form className="thankyou-box">
+      <form role="form" className="thankyou-box">
         <img src={illustrationThankYou} style={{ height: 100, width: 150 }} alt="Illustration-Thank-You" />
         <p className="rating-ty">You selected {ratingContext} out of 5</p>
         <h2 className="ty-h2">Thank you!</h2>
-        <p className="ty-content">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+        <p role="content" className="ty-content">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
       </form>
     </div>
   )
